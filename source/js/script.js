ef5adabff3119a0327da27dddec32a1eae9e84df
GuntyGunty <gunter0705@yandex.ru>
2020-07-28 16:00:17 +0300

    Добавляет анимацию появления блоков

--- source/js/script.js
+++ source/js/script.js
@@ -23,7 +23,14 @@ const fullPageScroll = new FullPageScroll();
 fullPageScroll.init();
 
 const body = document.querySelector(`body`);
+const lastRulesItem = document.querySelector(".rules__item:last-child");
+const rulesButtonElement = document.querySelector(".rules__link");
 
 body.onload = function () {
   body.classList.add(`loaded`);
 };
+
+lastRulesItem.onanimationend = function(event) {
+  rulesButtonElement.classList.add('appear');
+}
+
